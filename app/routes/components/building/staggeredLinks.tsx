1a73826eb325865248f1440a64d0d03392d8e9f5
Evan Marie Carr <evanmariecarr@gmail.com>
2024-10-25 20:05:18 -0400

    link menu

--- app/routes/components/building/staggeredLinks.tsx
+++ app/routes/components/building/staggeredLinks.tsx
@@ -1,6 +1,10 @@
 import { motion } from "framer-motion";
+import { CSSProperties } from "react";
+import { PiFlowerLotus } from "react-icons/pi";
+import Icon from "~/buildingBlockComponents/icon";
 import {
   Flex,
+  HStack,
   transitionVariants,
   VStack,
 } from "~/buildingBlockComponents/mainContainers";
@@ -15,26 +19,36 @@ export default function StaggeredLinks({
   links,
   direction = "flex-col",
   initialDelay = 1.5,
-  textClassName,
+  textClassName = "transition-300",
+  iconClassName = "group-hover:text-cyan-300",
   gap = "gap-1.5vh",
-  hoverClassName,
   animationVariants = "fadeSlideInRightQuarter",
   staggerDelay = 0.1,
   containerClassName = "items-end",
+  textContainerClassName,
+  hoverIcon = PiFlowerLotus,
+  buttonClassName = "hover:border-b-cyan-300 rounded-none transition-300 hover:text-cyan-300",
 }: {
   links: StaggeredLinkType[];
   direction?: string;
   initialDelay?: number;
   textClassName?: string;
+  iconClassName?: string;
+  textContainerClassName?: string;
   gap?: string;
-  hoverClassName?: string;
   staggerDelay?: number;
   containerClassName?: string;
   animationVariants?: keyof typeof transitionVariants;
+  buttonClassName?: string;
+  hoverIcon?: React.ComponentType<{
+    className?: string;
+    tabIndex?: number;
+    style?: CSSProperties;
+  }>;
 }) {
   const variants = transitionVariants[animationVariants];
   return (
-    <Flex className={`${gap} ${direction} ${containerClassName}`}>
+    <Flex className={`${gap} ${direction} ${containerClassName} `}>
       {links.map((link, index) => (
         <motion.button
           key={index}
@@ -45,9 +59,19 @@ export default function StaggeredLinks({
             type: "spring",
             stiffness: 44,
           }}
-          className={`${textClassName} ${hoverClassName}`}
+          className={`${buttonClassName} group hover:cursor-pointer border-transparent border-[0.2vh]`}
         >
-          {link.text}
+          <HStack className="items-center hover:cursor-pointer" gap="gap-1vh">
+            {hoverIcon && (
+              <Icon
+                icon={hoverIcon}
+                iconClassName={`text-transparent ${iconClassName} transition-300`}
+              />
+            )}
+            <Flex className={textContainerClassName}>
+              <span className={textClassName}>{link.text}</span>
+            </Flex>
+          </HStack>
         </motion.button>
       ))}
     </Flex>
