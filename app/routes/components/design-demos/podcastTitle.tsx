6b899d364891e0b504a555d2f9f2bf410cee689a
Evan Marie Carr <evanmariecarr@gmail.com>
2024-10-27 10:48:55 -0400

    pod

--- app/routes/components/design-demos/podcastTitle.tsx
+++ app/routes/components/design-demos/podcastTitle.tsx
@@ -6,10 +6,7 @@ import ImageWithTextOverlay from "../building/imageWithTextOverlay";
 export default function PodCastTitle() {
   return (
     <Box className="w-full relative py-5vh h-fit">
-      <FlexFull
-        className="flex-col items-center gap-2vh md:flex-row md:justify-center h-full"
-        gap="gap-3vh"
-      >
+      <FlexFull className="flex-col items-center gap-2vh h-full" gap="gap-3vh">
         <Flex className="md:hidden">
           <ImageWithTextOverlay width="w-80vw" height="h-80vw" />
         </Flex>
@@ -25,9 +22,6 @@ export default function PodCastTitle() {
             className="h-full"
           />
         </ScrollTransition>{" "}
-        <Flex className="hidden md:flex md:absolute translate-x-[13vh]">
-          <ImageWithTextOverlay width="w-60vh" height="h-60vh" />
-        </Flex>
       </FlexFull>
     </Box>
   );
