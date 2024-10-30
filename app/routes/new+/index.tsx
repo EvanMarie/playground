484d1931819e86d89514d2e86fd88463d0db58bc
Evan Marie Carr <evanmariecarr@gmail.com>
2024-10-30 12:27:40 -0400

    nav

--- app/routes/new+/index.tsx
+++ app/routes/new+/index.tsx
@@ -1,17 +1,22 @@
-import { FlexFull } from "~/buildingBlockComponents/mainContainers";
+import { Flex, FlexFull } from "~/buildingBlockComponents/mainContainers";
 import MainIndexContainer from "../building/mainIndexContainer";
 import SnapScrollSlideInContainer from "./components/snapScrollSlideInContainer";
 import { motion } from "framer-motion";
-import { NavLink } from "@remix-run/react";
+import { NavLink, useLocation } from "@remix-run/react";
+import { useScrollToHash } from "~/utils/useScrollToHash";
 
 export default function New() {
   function NavButton({ id, label }: { id: string; label: string }) {
+    const location = useLocation();
+    const currentHash = location.hash ? location.hash.substring(1) : "";
+    const isCurrent = currentHash === id.substring(1);
+
     return (
       <NavLink to={id}>
         <motion.div
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
-          className="bg-blue-500"
+          className={`${isCurrent ? "bg-pink-400" : "bg-blue-500"} w-full`}
         >
           {label}
         </motion.div>
@@ -20,12 +25,14 @@ export default function New() {
   }
   return (
     <>
-      <FlexFull className="flex-col h-fit absolute right-0.5vh top-1/2">
-        <motion.button>One</motion.button> <motion.button>Two</motion.button>{" "}
-        <motion.button>Three</motion.button>
-        <motion.button>Four</motion.button> <motion.button>Five</motion.button>
-        <motion.button>Six</motion.button>
-      </FlexFull>
+      <Flex className="flex-col gap-2vh h-fit fixed right-0.5vh top-1/2 z-10">
+        <NavButton id="#one" label="One" />
+        <NavButton id="#two" label="Two" />
+        <NavButton id="#three" label="Three" />
+        <NavButton id="#four" label="Four" />
+        <NavButton id="#five" label="Five" />
+        <NavButton id="#six" label="Six" />
+      </Flex>
       <MainIndexContainer className="snap-y snap-mandatory">
         <SnapScrollSlideInContainer id="one">
           <h2 className="text-sky-800">One</h2>
