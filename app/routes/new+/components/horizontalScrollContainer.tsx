33fdbf4540c3b0fcfab69d089e07b460f7cd2dd2
Evan Marie Carr <evanmariecarr@gmail.com>
2024-11-04 22:52:21 -0500

    scroll

--- app/routes/new+/components/horizontalScrollContainer.tsx
+++ app/routes/new+/components/horizontalScrollContainer.tsx
@@ -1,4 +1,9 @@
-import { Box, FlexFull } from "~/buildingBlockComponents/mainContainers";
+import {
+  Box,
+  FlexFull,
+  HStack,
+} from "~/buildingBlockComponents/mainContainers";
+import { motion } from "framer-motion";
 
 export default function HorizontalScrollContainer({
   children,
@@ -13,8 +18,16 @@ export default function HorizontalScrollContainer({
   padding?: string;
   gap?: string;
 }) {
+  const handleScrollLeft = () => {
+    console.log("scroll left");
+  };
+
+  const handleScrollRight = () => {
+    console.log("scroll right");
+  };
+
   return (
-    <Box className={`${width} overflow-hidden ${height}`}>
+    <Box className={`${width} overflow-hidden ${height} relative pb-4svh`}>
       <Box
         className={`${width} overflow-x-auto overflow-y-hidden h-full hide-scrollbar`}
       >
@@ -24,6 +37,12 @@ export default function HorizontalScrollContainer({
           {children}
         </FlexFull>
       </Box>
+      <FlexFull className="absolute bottom-0 right-0 left-0 h-4svh justify-center">
+        <HStack gap="gap-10vh">
+          <motion.button>Left</motion.button>
+          <motion.button>Right</motion.button>
+        </HStack>
+      </FlexFull>
     </Box>
   );
 }
