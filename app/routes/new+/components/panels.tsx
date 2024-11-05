cf981e5f0cdd37261db72a4bb81a9dd0464d338c
Evan Marie Carr <evanmariecarr@gmail.com>
2024-11-05 17:53:11 -0500

    components

--- app/routes/new+/components/panels.tsx
+++ app/routes/new+/components/panels.tsx
@@ -13,7 +13,7 @@ import PanelShiftReveal from "./panelShiftReveal";
 export function PanelOne() {
   return (
     <PanelShiftReveal
-      sectionClassName="border-fuchsia-300 border-[0.2vh]"
+      sectionClassName="border-fuchsia-300 border-[0.2vh] shadowNarrowLooser border-[0.2vh]"
       duration={0.5}
     />
   );
@@ -25,32 +25,32 @@ export function PanelTwo() {
       <VStackFull>
         <HorizontalScrollContainer>
           <Box className="p-1vh">
-            <Center className="w-40vw h-80vh bg-indigo-500 flex-shrink-0">
+            <Center className="w-40vw h-80vh bg-fuchsia-300/70 border-900-md shadowNarrowNormal flex-shrink-0">
               1
             </Center>
           </Box>
           <Box className="p-1vh">
-            <Center className="w-40vw h-80vh bg-indigo-500 flex-shrink-0">
+            <Center className="w-40vw h-80vh bg-fuchsia-300/70 border-900-md shadowNarrowNormal flex-shrink-0">
               2
             </Center>
           </Box>
           <Box className="p-1vh">
-            <Center className="w-40vw h-80vh bg-indigo-500 flex-shrink-0">
+            <Center className="w-40vw h-80vh bg-fuchsia-300/70 border-900-md shadowNarrowNormal flex-shrink-0">
               3
             </Center>
           </Box>
           <Box className="p-1vh">
-            <Center className="w-40vw h-80vh bg-indigo-500 flex-shrink-0">
+            <Center className="w-40vw h-80vh bg-fuchsia-300/70 border-900-md shadowNarrowNormal flex-shrink-0">
               4
             </Center>
           </Box>
           <Box className="p-1vh">
-            <Center className="w-40vw h-80vh bg-indigo-500  flex-shrink-0">
+            <Center className="w-40vw h-80vh bg-fuchsia-300/70 border-900-md shadowNarrowNormal  flex-shrink-0">
               5
             </Center>
           </Box>
           <Box className="p-1vh">
-            <Center className="w-40vw h-80vh bg-indigo-500  flex-shrink-0">
+            <Center className="w-40vw h-80vh bg-fuchsia-300/70 border-900-md shadowNarrowNormal  flex-shrink-0">
               6
             </Center>
           </Box>
@@ -62,21 +62,51 @@ export function PanelTwo() {
 
 export function PanelThree() {
   const contents = [
-    <Center className="w-full h-full bg-indigo-500">1</Center>,
-    <Center className="w-full h-full bg-indigo-500">2</Center>,
-    <Center className="w-full h-full bg-indigo-500">3</Center>,
-    <Center className="w-full h-full bg-indigo-500">4</Center>,
-    <Center className="w-full h-full bg-indigo-500">5</Center>,
-    <Center className="w-full h-full bg-indigo-500">6</Center>,
-    <Center className="w-full h-full bg-indigo-500">7</Center>,
-    <Center className="w-full h-full bg-indigo-500">8</Center>,
-    <Center className="w-full h-full bg-indigo-500">9</Center>,
-    <Center className="w-full h-full bg-indigo-500">10</Center>,
-    <Center className="w-full h-full bg-indigo-500">11</Center>,
-    <Center className="w-full h-full bg-indigo-500">12</Center>,
-    <Center className="w-full h-full bg-indigo-500">13</Center>,
-    <Center className="w-full h-full bg-indigo-500">14</Center>,
-    <Center className="w-full h-full bg-indigo-500">15</Center>,
+    <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+      1
+    </Center>,
+    <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+      2
+    </Center>,
+    <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+      3
+    </Center>,
+    <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+      4
+    </Center>,
+    <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+      5
+    </Center>,
+    <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+      6
+    </Center>,
+    <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+      7
+    </Center>,
+    <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+      8
+    </Center>,
+    <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+      9
+    </Center>,
+    <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+      10
+    </Center>,
+    <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+      11
+    </Center>,
+    <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+      12
+    </Center>,
+    <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+      13
+    </Center>,
+    <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+      14
+    </Center>,
+    <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+      15
+    </Center>,
   ];
   return (
     <CenterFull>
@@ -91,21 +121,51 @@ export function PanelFour() {
       <VStackFull>
         <PopUpContent
           contents={[
-            <Center className="w-full h-full bg-indigo-500">1</Center>,
-            <Center className="w-full h-full bg-indigo-500">2</Center>,
-            <Center className="w-full h-full bg-indigo-500">3</Center>,
-            <Center className="w-full h-full bg-indigo-500">4</Center>,
-            <Center className="w-full h-full bg-indigo-500">5</Center>,
-            <Center className="w-full h-full bg-indigo-500">6</Center>,
-            <Center className="w-full h-full bg-indigo-500">7</Center>,
-            <Center className="w-full h-full bg-indigo-500">8</Center>,
-            <Center className="w-full h-full bg-indigo-500">9</Center>,
-            <Center className="w-full h-full bg-indigo-500">10</Center>,
-            <Center className="w-full h-full bg-indigo-500">11</Center>,
-            <Center className="w-full h-full bg-indigo-500">12</Center>,
-            <Center className="w-full h-full bg-indigo-500">13</Center>,
-            <Center className="w-full h-full bg-indigo-500">14</Center>,
-            <Center className="w-full h-full bg-indigo-500">15</Center>,
+            <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+              1
+            </Center>,
+            <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+              2
+            </Center>,
+            <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+              3
+            </Center>,
+            <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+              4
+            </Center>,
+            <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+              5
+            </Center>,
+            <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+              6
+            </Center>,
+            <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+              7
+            </Center>,
+            <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+              8
+            </Center>,
+            <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+              9
+            </Center>,
+            <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+              10
+            </Center>,
+            <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+              11
+            </Center>,
+            <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+              12
+            </Center>,
+            <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+              13
+            </Center>,
+            <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+              14
+            </Center>,
+            <Center className="w-full h-full bg-fuchsia-300/70 border-900-md shadowNarrowNormal">
+              15
+            </Center>,
           ]}
         />
       </VStackFull>
